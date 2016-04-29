7788a03e5b824e3d3ed022b59d200f10f0fdc925
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-04-29 01:23:54 -0400

    add yt_data utility

--- sitebuilder.py
+++ sitebuilder.py
@@ -3,10 +3,14 @@ import sys
 from flask import Flask, render_template, render_template_string, Markup
 from flask_flatpages import FlatPages, pygmented_markdown
 from flask_frozen import Freezer
-from configparser import ConfigParser
+from YTCrawl import crawler
+import csv
+from StringIO import StringIO
+import datetime as dt
+
 
 DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
+# FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
@@ -30,6 +34,29 @@ def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
 
+@app.route('/yt_data/<string:vid_id>/')
+def yt_data(vid_id):
+    c = crawler.Crawler()
+    try:
+        results = c.single_crawl(vid_id)
+    except Exception, e:
+        return str(e)
+
+    headers = ['date', 'dailyViewcount'] + [item for item in ['numShare', 'numSubscriber'] if
+                                     results.get(item, False)]
+
+    date = results['uploadDate']
+    outstrIO = StringIO()
+    writer = csv.writer(outstrIO, delimiter=',')
+    writer.writerow(headers)
+    for i, count in enumerate(results['dailyViewcount']):
+        row = [date.strftime('%Y-%m-%d'), count] + [results[header][i] for header in headers[2:]]
+        writer.writerow(row)
+        date += dt.timedelta(1)
+
+    raw_str = outstrIO.getvalue()
+    print(raw_str)
+    return raw_str.replace('\n', '<br />')
 
 def prerender_jinja(text):
     prerendered_body = render_template_string(Markup(text))
@@ -39,7 +66,11 @@ app.config['FLATPAGES_HTML_RENDERER'] = prerender_jinja
 
 
 if __name__ == '__main__':
+    # c = crawler.Crawler()
+    # results = c.single_crawl('CleR2nYASdo')
+    # print results
+    # print len(results['dailyViewcount'])
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
         freezer.freeze()
     else:
-        app.run(host='0.0.0.0', port=8000)
+        app.run(port=8000, host='0.0.0.0')
